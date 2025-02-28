b539409c9f8bf5ef35b69ab6ee8d1545d65adbe6
JungSik Jeong <jungsik.jeong@lcnine.kr>
2025-03-01 02:06:35 +0900

    테일윈드 적용완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <div>
+    <div className='@container-normal bg-amber-400 w-2xl'>
       <h1 className='text-red-500'>Hello Word</h1>
     </div>
   );
