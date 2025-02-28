2698bc7025210e268d54101928854af9794e716a
JungSik Jeong <wndtlr1024@gmail.com>
2025-02-28 19:47:17 +0900

    supabase 설정 (미흡)

---
+++ src/lib/supabase/client.ts
@@ -0,0 +1,34 @@
+todo: Database;
+
+import {
+  createClient as createSupabaseClient,
+  SupabaseClient,
+} from '@supabase/supabase-js';
+import { useMemo } from 'react';
+
+let client: SupabaseClient<Database> | undefined;
+
+function getSupabaseBrowserClient() {
+  if (client) {
+    return client;
+  }
+
+  client = createSupabaseClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      auth: {
+        persistSession: true, // 세션 유지 활성화
+        autoRefreshToken: true, // 토큰 자동 갱신 활성화
+      },
+    }
+  );
+
+  return client;
+}
+
+function useCreateClient() {
+  return useMemo(getSupabaseBrowserClient, []);
+}
+
+export default useCreateClient;
