4377916079812bc1be1837ee618f93d305647499
JungSik Jeong <jungsik.jeong@lcnine.kr>
2025-03-01 04:30:49 +0900

    테일윈드, shadcnui설정 vite.config 오류 해결

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div className='@container-normal bg-amber-400 w-2xl'>
-      <h1 className='text-red-500'>Hello Word</h1>
-    </div>
-  );
-}
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import { Button } from '@/components/ui/button';
+
+export default function App() {
+  return <div className='@container-normal bg-amber-400 w-2xl'></div>;
+}
