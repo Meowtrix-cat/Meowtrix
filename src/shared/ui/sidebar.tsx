4cd2ca8fcf5aed05440c86082d5974daa272f501
JungSik Jeong <jungsik.jeong@lcnine.kr>
2025-03-02 22:41:35 +0900

    hooks 폴더 변경

--- src/shared/ui/sidebar.tsx
+++ src/shared/ui/sidebar.tsx
@@ -22,7 +22,7 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from './tooltip';
-import { useIsMobile } from '@/hooks';
+import { useIsMobile } from '@/shared/hooks';
 
 const SIDEBAR_COOKIE_NAME = 'sidebar_state';
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7;
